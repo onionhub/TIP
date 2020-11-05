b5f85b31700b085d270efa0ae468a72f3c3b5028
Ehsun Assadi <35137611+onionhub@users.noreply.github.com>
2020-11-05 13:21:31 +0330

    Update and rename Edge-Preserving_Smoothing.m to Edge-Preserving_Denoising.m

--- Matlab/Edge-Preserving_Denoising.m
+++ Matlab/Edge-Preserving_Denoising.m
@@ -1,8 +1,7 @@
+%%% This function is suitable for denoising noisy images.
 % StepSize=1/7 is more stable for image denoising purpose.
-% But, the StepSize plays a major role for noise free image smoothing purpose. For example StepSize=1/3 will act different from StepSize=1/7
-%Important: C_l cannot be negative one as it was for image denoising purpose. For convenience, it is better to consider C_l zero for noise free image smoothing purpose.
-%sigma0 is the variance of u_g in line 64.
-function [ue] = Edge-Preserving_Smoothing(NoisyIm,sigma0,sigma1,NumIter,StepSize)
+%sigma0 is the variance of u_g in lines 32 and 64.
+function [ue] = Edge-Preserving_Denoising(NoisyIm,sigma0,sigma1,NumIter,StepSize)
 Input=double(NoisyIm);
 [m,n,c]=size(Input);
 Out=zeros(m,n,c);
